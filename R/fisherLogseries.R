2c5e8854da53e091c887eb24f2c3ce2542802d09
ajrominger <ajrominger@gmail.com>
2016-03-10 17:54:18 -0800

    fourced p funciton to only take integer input for `n'

--- R/fisherLogseries.R
+++ R/fisherLogseries.R
@@ -19,9 +19,24 @@ dfish <- function(n, beta, log=FALSE) {
 }
 
 pfish <- function(n, beta, log=FALSE) {
-    1 + .betax(exp(-beta), n+1, 0) / log(1 - exp(-beta))
+    out <- 1 + .betax(exp(-beta), n+1, 0) / log(1 - exp(-beta))
+    
+    if(any(n %% 1 != 0)) {
+        for(bad in n[n %% 1 != 0]) {
+            warning(sprintf('non-integer n = %s', bad))
+        }
+        
+        out[n %% 1 != 0] <- 0
+    }
+    
+    if(log) out <- log(out)
+    return(out)
 }
 
 .betax <- function(x, a, b) {
     1/a * x^a * (1-x)^b * gsl::hyperg_2F1(a+b, 1, a+1, x)
+}
+
+.fishcdf <- function(n, beta) {
+    1 + .betax(exp(-beta), n+1, 0) / log(1 - exp(-beta))
 }
