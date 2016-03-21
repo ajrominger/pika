a07a099a4fd8b20b62594daee6a06a7143ab596d
ajrominger <ajrominger@gmail.com>
2016-03-20 20:05:00 -0700

    added help doc syntax

--- R/fisherLogseries.R
+++ R/fisherLogseries.R
@@ -1,17 +1,37 @@
-## ==============================================
-## distribution functions for Fisher's log series
-## re-parameterized as in Harte and others
-## ==============================================
+#' @title Fisher's logseries
+#'  
+#' @description \code{dfish} gives the probability mass function, \code{pfish} gives the cumulative mass 
+#' function, \code{qfish} the quantile function, \code{rfish} randome number generation
+#' 
+#' @details
+#' 
+#' @param x
+#' @param beta
+#' @param log
+#' @param n
+#' 
+#' @keywords Fisher logseries, species abundance, SAD
+#' @export
+#' 
+#' @examples
+#' 
+#' dfish(1:10, 0.01)
+#' 
+#' @return A numeric vector of length equal to the input
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#' @seealso 
+#' @references 
 
-dfish <- function(n, beta, log=FALSE) {
-    out <- 1/log(1/(1-exp(-beta))) * exp(-beta*n)/n
+dfish <- function(x, beta, log=FALSE) {
+    out <- 1/log(1/(1-exp(-beta))) * exp(-beta*x)/x
     
-    if(any(n %% 1 != 0)) {
-        for(bad in n[n %% 1 != 0]) {
+    if(any(x %% 1 != 0)) {
+        for(bad in x[x %% 1 != 0]) {
             warning(sprintf('non-integer n = %s', bad))
         }
         
-        out[n %% 1 != 0] <- 0
+        out[x %% 1 != 0] <- 0
     }
     
     if(log) out <- log(out)
