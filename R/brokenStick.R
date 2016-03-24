d0d7dbc16ed5c7bc2702a2c2eee268cd48b3203a
ajrominger <ajrominger@gmail.com>
2016-03-24 12:38:05 -0700

    functions for broken stick (=geometric)

---
+++ R/brokenStick.R
@@ -0,0 +1,107 @@
+#' @title Broken stick
+#'  
+#' @description \code{dstick} gives the probability mass function, \code{pstick} gives the cumulative mass 
+#' function, \code{qstick} the quantile function, \code{rstick} randome number generation
+#' 
+#' @details These functions assume infinite support of the Broken stick
+#' 
+#' @param x vector of integers for which to return the probability
+#' @param q vector of integers for which to return the cumulative probability
+#' @param p vector of probabilities for which to return the quantile
+#' @param n number of random replicates
+#' @param r parameter of the broken stick model, see Details
+#' @param log logical, should the log probability be used
+#' @param lower.tail logical, should the lower tail be used
+#' 
+#' @keywords Fisher logseries, species abundance, SAD
+#' @export
+#' 
+#' @examples
+#' 
+#' dfish(1:10, 0.01)
+#' 
+#' @return A numeric vector of length equal to the input
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+# @seealso 
+# @references 
+
+#' @rdname Fisher
+
+dfish <- function(x, beta, log=FALSE) {
+    out <- 1/log(1/(1-exp(-beta))) * exp(-beta*x)/x
+    
+    if(any(x %% 1 != 0)) {
+        for(bad in x[x %% 1 != 0]) {
+            warning(sprintf('non-integer x = %s', bad))
+        }
+        
+        out[x %% 1 != 0] <- 0
+    }
+    
+    if(log) out <- log(out)
+    return(out)
+}
+
+
+#' @rdname Fisher
+
+pfish <- function(q, beta, lower.tail=TRUE, log=FALSE) {
+    out <- 1 + .betax(exp(-beta), q+1, 0) / log(1 - exp(-beta))
+    
+    if(any(q %% 1 != 0)) {
+        for(bad in q[q %% 1 != 0]) {
+            warning(sprintf('non-integer q = %s', bad))
+        }
+        
+        out[q %% 1 != 0] <- 0
+    }
+    
+    if(lower.tail) out <- 1 - out
+    if(log) out <- log(out)
+    return(out)
+}
+
+#' @rdname Fisher
+
+qfish <- function(p, beta, lower.tail=TRUE, log=FALSE) {
+    if(log) p <- exp(p)
+    if(lower.tail) p <- 1 - p
+    
+    out <- .fishcdfinv(p, beta)
+    
+    if(any(is.nan(out))) {
+        warning('NaNs produced')
+    }
+    
+    return(out)
+}
+
+
+#' @rdname Fisher
+
+rfish <- function(n, beta) {
+    r <- runif(n)
+    
+    return(.fishcdfinv(r, beta))
+}
+
+## =================================
+## helper functions
+## =================================
+
+## solution to the beta function using the hypergeometic for better accuracy
+.betax <- function(x, a, b) {
+    1/a * x^a * (1-x)^b * gsl::hyperg_2F1(a+b, 1, a+1, x)
+}
+
+## cdf of the fisher log series
+.fishcdf <- function(x, beta) {
+    1 + .betax(exp(-beta), x+1, 0) / log(1 - exp(-beta))
+}
+
+## inverse cdf of the fisher log series
+.fishcdfinv <- function(p, beta) {
+    approx(x=.fishcdf(1:10000, beta), y=1:10000, xout=p, method='constant', 
+           yleft=NaN, yright=NaN, f=0)
+}
