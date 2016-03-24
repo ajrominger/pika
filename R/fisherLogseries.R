7c605273939304a0fc6f6f4d5f897fbba5ec21ef
ajrominger <ajrominger@gmail.com>
2016-03-24 11:42:18 -0700

    added warning messages and q function

--- R/fisherLogseries.R
+++ R/fisherLogseries.R
@@ -3,7 +3,7 @@
 #' @description \code{dfish} gives the probability mass function, \code{pfish} gives the cumulative mass 
 #' function, \code{qfish} the quantile function, \code{rfish} randome number generation
 #' 
-#' @details
+#' @details These functions assume infinite support of the Fisher logseries
 #' 
 #' @param x
 #' @param beta
@@ -20,15 +20,15 @@
 #' @return A numeric vector of length equal to the input
 #'
 #' @author Andy Rominger <ajrominger@@gmail.com>
-#' @seealso 
-#' @references 
+# @seealso 
+# @references 
 
 dfish <- function(x, beta, log=FALSE) {
     out <- 1/log(1/(1-exp(-beta))) * exp(-beta*x)/x
     
     if(any(x %% 1 != 0)) {
         for(bad in x[x %% 1 != 0]) {
-            warning(sprintf('non-integer n = %s', bad))
+            warning(sprintf('non-integer x = %s', bad))
         }
         
         out[x %% 1 != 0] <- 0
@@ -38,25 +38,40 @@ dfish <- function(x, beta, log=FALSE) {
     return(out)
 }
 
-pfish <- function(n, beta, log=FALSE) {
-    out <- 1 + .betax(exp(-beta), n+1, 0) / log(1 - exp(-beta))
+
+#' @rdname dfish
+
+pfish <- function(q, beta, lower.tail=TRUE, log=FALSE) {
+    out <- 1 + .betax(exp(-beta), q+1, 0) / log(1 - exp(-beta))
     
-    if(any(n %% 1 != 0)) {
-        for(bad in n[n %% 1 != 0]) {
-            warning(sprintf('non-integer n = %s', bad))
+    if(any(q %% 1 != 0)) {
+        for(bad in q[q %% 1 != 0]) {
+            warning(sprintf('non-integer q = %s', bad))
         }
         
-        out[n %% 1 != 0] <- 0
+        out[q %% 1 != 0] <- 0
     }
     
+    if(lower.tail) out <- 1 - out
     if(log) out <- log(out)
     return(out)
 }
+
+#' @rdname dfish
+
+qfish <- function(p, beta, lower.tail=TRUE, log=FALSE) {
+    approx(x=.fishcdf(1:10000, beta), y=1:10000, xout=p, method='constant', yleft=NaN, yright=NaN, f=0)
+}
+
+## =================================
+## helper functions
+## =================================
 
 .betax <- function(x, a, b) {
     1/a * x^a * (1-x)^b * gsl::hyperg_2F1(a+b, 1, a+1, x)
 }
 
-.fishcdf <- function(n, beta) {
-    1 + .betax(exp(-beta), n+1, 0) / log(1 - exp(-beta))
+.fishcdf <- function(x, beta) {
+    1 + .betax(exp(-beta), x+1, 0) / log(1 - exp(-beta))
 }
+
