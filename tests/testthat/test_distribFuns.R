a98e63d3270b3907a748ed0cd5ff08e64c72e8af
ajrominger <ajrominger@gmail.com>
2016-03-28 14:27:38 -0700

    fixed tests for ptpois and added test for dtpois

--- tests/testthat/test_distribFuns.R
+++ tests/testthat/test_distribFuns.R
@@ -5,14 +5,15 @@ context('distribution functions work')
 ## =================================
 
 test_that('truncated Poisson dfun works', {
+    expect_equal(dtpois(1:10, 1), dpois(1:10, 1) / (1 - dpois(0, 1)))
     expect_warning(dtpois(0.5, 1))
     expect_equal(dtpois(0, 1), 0)
 })
 
 test_that('truncated Poisson pfun works', {
-    expect_true(all(ptpois(1:10, 1) - cumsum(dtpois(1:10, 1)) < .Machine$double.eps))
-    expect_true(all(ptpois(1:10, 1, log=TRUE) - log(cumsum(dtpois(1:10, 1))) < .Machine$double.eps))
-    expect_true(all(ptpois(1:10, 1, lower.tail=FALSE) - (1 - cumsum(dtpois(1:10, 1))) < .Machine$double.eps))
+    expect_true(all(abs(ptpois(1:10, 1) - cumsum(dtpois(1:10, 1))) < .Machine$double.eps))
+    expect_true(all(abs(ptpois(1:10, 1, log=TRUE) - log(cumsum(dtpois(1:10, 1)))) < .Machine$double.eps))
+    expect_true(all(abs(ptpois(1:10, 1, lower.tail=FALSE) - (1 - cumsum(dtpois(1:10, 1)))) < .Machine$double.eps))
     expect_equal(ptpois(0, 1), 0)
 })
 
