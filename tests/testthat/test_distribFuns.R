2e2391db01fc95c08a3789464b68a5bb04e0afd9
ajrominger <ajrominger@gmail.com>
2016-03-28 13:55:06 -0700

    added tests of qtpois and rtpois

--- tests/testthat/test_distribFuns.R
+++ tests/testthat/test_distribFuns.R
@@ -1,5 +1,9 @@
 context('distribution functions work')
 
+## =================================
+## Truncated Poisson
+## =================================
+
 test_that('truncated Poisson dfun works', {
     expect_warning(dtpois(0.5, 1))
     expect_equal(dtpois(0, 1), 0)
@@ -10,4 +14,16 @@ test_that('truncated Poisson pfun works', {
     expect_true(all(ptpois(1:10, 1, log=TRUE) - log(cumsum(dtpois(1:10, 1))) < .Machine$double.eps))
     expect_true(all(ptpois(1:10, 1, lower.tail=FALSE) - (1 - cumsum(dtpois(1:10, 1))) < .Machine$double.eps))
     expect_equal(ptpois(0, 1), 0)
+})
+
+test_that('truncated Poisson qfun works', {
+    expect_equal(qtpois(ptpois(1:30, 10), 10), 1:30)
+    expect_equal(qtpois(ptpois(1:30, 10, lower.tail=FALSE), 10, lower.tail=FALSE), 1:30)
+    expect_equal(qtpois(ptpois(1:30, 10, log=TRUE), 10, log=TRUE), 1:30)
+})
+
+test_that('truncated Poisson rfun works', {
+    m <- 2
+    r1 <- rtpois(10000, m)
+    expect_true(abs(mean(r1) - m / (1 - dpois(0, m))) < 0.05)
 })
