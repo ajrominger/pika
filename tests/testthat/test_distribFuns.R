9862d5204b7f9eb44b98cc43dc109a852590c9ba
ajrominger <ajrominger@gmail.com>
2016-03-28 15:12:42 -0700

    added neg binom tests

--- tests/testthat/test_distribFuns.R
+++ tests/testthat/test_distribFuns.R
@@ -28,3 +28,33 @@ test_that('truncated Poisson rfun works', {
     r1 <- rtpois(10000, m)
     expect_true(abs(mean(r1) - m / (1 - dpois(0, m))) < 0.05)
 })
+
+
+## =================================
+## Truncated Negative Binomial
+## =================================
+
+test_that('truncated Poisson dfun works', {
+    expect_equal(dtpois(1:10, 1), dpois(1:10, 1) / (1 - dpois(0, 1)))
+    expect_warning(dtpois(0.5, 1))
+    expect_equal(dtpois(0, 1), 0)
+})
+
+test_that('truncated Poisson pfun works', {
+    expect_true(all(abs(ptpois(1:10, 1) - cumsum(dtpois(1:10, 1))) < .Machine$double.eps))
+    expect_true(all(abs(ptpois(1:10, 1, log=TRUE) - log(cumsum(dtpois(1:10, 1)))) < .Machine$double.eps))
+    expect_true(all(abs(ptpois(1:10, 1, lower.tail=FALSE) - (1 - cumsum(dtpois(1:10, 1)))) < .Machine$double.eps))
+    expect_equal(ptpois(0, 1), 0)
+})
+
+test_that('truncated Poisson qfun works', {
+    expect_equal(qtpois(ptpois(1:30, 10), 10), 1:30)
+    expect_equal(qtpois(ptpois(1:30, 10, lower.tail=FALSE), 10, lower.tail=FALSE), 1:30)
+    expect_equal(qtpois(ptpois(1:30, 10, log=TRUE), 10, log=TRUE), 1:30)
+})
+
+test_that('truncated Negative Binomial rfun works', {
+    m <- 2
+    r1 <- rtnegb(10000, m, 3)
+    expect_true(abs(mean(r1) - m / (1 - dtnegb(0, m, 3))) < 0.05)
+})
