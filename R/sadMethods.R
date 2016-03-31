9d596a7465c113b095e76444b1cd351b88b56400
ajrominger <ajrominger@gmail.com>
2016-03-31 15:55:00 -0700

    added help docs to sad methods

--- R/sadMethods.R
+++ R/sadMethods.R
@@ -1,9 +1,53 @@
-
+#' @title Print method for objects of class \code{sad}
+#'  
+#' @description Print objects of class \code{sad}
+#' 
+#' @details See examples
+#' 
+#' @param x an object of class \code{sad}
+#' 
+#' @keywords species abundance, SAD
+#' @export
+#' 
+#' @examples
+#' x <- rtpois(1000, 2)
+#' sad1 <- fitSAD(x, models='tpois')
+#' print(sad1[[1]])
+#' 
+#' @return Silently returns \code{x}.
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#' @seealso fitSAD
+# @references
 
 print.sad <- function(x) {
     cat(sprintf('species abundance distribution modeled by "%s" with parameters \n', x$model))
     print(x$MLE)
+    invisible(x)
 }
+
+
+#' @title Log likelihood method for objects of class \code{sad}
+#'  
+#' @description Compute log likelihood for objects of class \code{sad}
+#' 
+#' @details See examples
+#' 
+#' @param x an object of class \code{sad}
+#' 
+#' @keywords species abundance, SAD
+#' @export
+#' 
+#' @examples
+#' x <- rtpois(1000, 2)
+#' sad1 <- fitSAD(x, models='tpois')
+#' logLik(sad1[[1]])
+#' 
+#' @return An object of class \code{logLik} with attributes "\code{df}" and "\code{nobs}".
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#' @seealso fitSAD
+# @references
 
 logLik.sad <- function(x) {
     out <- x$ll
@@ -13,6 +57,30 @@ logLik.sad <- function(x) {
     
     return(out)
 }
+
+
+
+#' @title Plot method for objects of class \code{sad}
+#'  
+#' @description Plot objects of class \code{sad}
+#' 
+#' @details See examples
+#' 
+#' @param x an object of class \code{sad}
+#' 
+#' @keywords species abundance, SAD
+#' @export
+#' 
+#' @examples
+#' x <- rtpois(1000, 2)
+#' sad1 <- fitSAD(x, models='tpois')
+#' plot(sad1[[1]])
+#' 
+#' @return \code{NULL}.
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#' @seealso fitSAD
+# @references
 
 plot.sad <- function(x, ptype=c('cdf', 'rad'), ...) {
     ptype <- match.arg(ptype, c('cdf', 'rad'))
@@ -27,11 +95,14 @@ plot.sad <- function(x, ptype=c('cdf', 'rad'), ...) {
         } else {
             plot(sort(x$data, decreasing=TRUE))
         }
-    } else {
-        
     }
     
-    if(!is.null(x$model)) {
-        if(ptype)
-    }
+#     if(!is.null(x$model)) {
+#         if(dev.cur() == 1) {
+#             plotfun <- function(x, y) plot(x, y, ...)
+#         } else {
+#             plotfun <- function(x, y) lines(x, y)
+#         }
+#         if(ptype)
+#     }
 }
