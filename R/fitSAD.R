53fb2460cfa25a89c5881e5611e521f4b9d82fd3
ajrominger <ajrominger@gmail.com>
2016-03-31 15:54:35 -0700

    fixed up help doc for fitSAD

--- R/fitSAD.R
+++ R/fitSAD.R
@@ -2,7 +2,18 @@
 #'  
 #' @description \code{fitSAD} uses maximum likelihood to fit one or several SAD models to data
 #' 
-#' @details The default behavior fits all models availible in \code{pika}
+#' @details The default behavior fits all models availible in \code{pika}:
+#' \describe{
+#'  \item{\code{fish}}{Fisher's log series}
+#'  \item{\code{plnorm}}{Poisson log normal}
+#'  \item{\code{stick}}{Broken stick}
+#'  \item{\code{tnegb}}{Truncated negative binomial}
+#'  \item{\code{tpois}}{Truncated Poisson}
+#' }
+#' Exact solutions are availible for \code{tpois} and \code{stick}. In the case of \code{fish}, 
+#' optimization is used to solve \eqn{\bar{x} = \frac{-1}{log(1-e^{-\beta})}\frac{e^{-\beta}}{1-e^{-\beta}}}. 
+#' For \code{plnorm} and \code{tnegb} optimization is used to maximize the log likelihood functions based 
+#' directly on \code{dplnorm} and \code{dtnegb}, respectively.
 #' 
 #' @param x vector of integers representing a sample of species abundances
 #' @param models character vector of models to be fit (can be more than one model, see examples)
@@ -11,13 +22,20 @@
 #' @export
 #' 
 #' @examples
+#' x <- rtpois(1000, 2)
+#' fitSAD(x, models='tpois')
 #' 
-#' dplnorm(1:10, 0.5, 0.1)
-#' 
-#' @return An object of class \code{logLik} with attributes "\code{df}" and "\code{nobs}."
+#' @return A list containing named objects of class \code{sad} with elements
+#' \describe{
+#'   \item{\code{MLE}}{The maximum likelihood estimate(s)}
+#'   \item{\code{ll}}{The maximized log likelihood}
+#'   \item{\code{df}}{The associated degrees of freedom}
+#'   \item{\code{nobs}}{The associated number of observations}
+#'   \item{\code{model}}{The name of the model fit}
+#' }
 #'
 #' @author Andy Rominger <ajrominger@@gmail.com>
-#' @seealso logLik
+#' @seealso logLik.sad, dfish, dplnorm, dstick, dtnegb, dtpois
 # @references 
 
 fitSAD <- function(x, models=c('fish', 'plnorm', 'stick', 'tnegb', 'tpois')) {
@@ -46,11 +64,9 @@ fitSAD <- function(x, models=c('fish', 'plnorm', 'stick', 'tnegb', 'tpois')) {
 }
 
 
-
 ## ========================================================
 ## helper functions to return likelihood estimates for SADs
 ## ========================================================
-
 
 ## MLE for fisher log series
 #' @export
@@ -86,7 +102,6 @@ fitSAD <- function(x, models=c('fish', 'plnorm', 'stick', 'tnegb', 'tpois')) {
     
     return(list(MLE=fit$par, ll=-fit$value, df=2, nobs=length(x)))
 }
-
 
 ## MLE for broken stick
 #' @export
