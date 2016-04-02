500358c2edebe7c77c243e9a0e731d77135f021e
ajrominger <ajrominger@gmail.com>
2016-04-01 21:49:52 -0700

    added mse and residuals tests

--- tests/testthat/test_sadMethods.R
+++ tests/testthat/test_sadMethods.R
@@ -18,7 +18,7 @@ test_that('sad2Rank works', {
 ## logLik
 ## =================================
 
-test_that('logLik.sad works', {
+test_that('logLik works', {
     la <- 2
     x <- rtpois(100, la)
     xfit <- fitSAD(x, 'tpois', keepData=TRUE)[[1]]
@@ -34,11 +34,29 @@ test_that('logLik.sad works', {
 ## residuals and mse
 ## =================================
 
-test_that('logLik.sad works', {
+test_that('residuals and mse work', {
     la <- 2
     x <- rtpois(100, la)
     xfit <- fitSAD(x, 'tpois', keepData=TRUE)[[1]]
-    expect_is(logLik(xfit), 'logLik')
+    resid1 <- residuals(xfit)
+    resid2 <- residuals(xfit, type='cumulative')
+    resid3 <- residuals(xfit, log=TRUE, relative=FALSE)
+    resid4 <- residuals(xfit, log=FALSE, relative=TRUE)
+    resid5 <- residuals(xfit, type='cumulative', log=TRUE, relative=TRUE)
+    resid6 <- residuals(xfit, type='cumulative', log=FALSE, relative=TRUE)
+    
+    expect_equal(length(resid1), 100)
+    expect_equal(mse(xfit), mean(resid1^2))
+    expect_equal(mse(xfit, type='cumulative'), mean(resid2^2))
+    expect_equal(mse(xfit, log=TRUE, relative=FALSE), mean(resid3^2))
+    expect_equal(mse(xfit, log=FALSE, relative=TRUE), mean(resid4^2))
+    expect_equal(mse(xfit, type='cumulative', log=TRUE, relative=TRUE), mean(resid5^2))
+    expect_equal(mse(xfit, type='cumulative', log=FALSE, relative=TRUE), mean(resid6^2))
+    
+    z <- mseZ(xfit, nrep=10, return.sim=TRUE, type='cumulative', log=FALSE, relative=TRUE)
+    expect_equal(length(z), 3)
+    expect_equal(z$obs, mse(xfit, type='cumulative', log=FALSE, relative=TRUE))
+    expect_equal(length(z$sim), 10)
 })
 
 
